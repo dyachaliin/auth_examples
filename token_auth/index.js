7665966b7acca9a35b9f37117734cbedbe5f5c82
Alina Diachenko <dyachaliin@gmail.com>
2023-12-24 19:07:10 +0200

    Modified application to make redirection to auth0 login page

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,152 +1,87 @@
-const uuid = require('uuid');
 const express = require('express');
-const onFinished = require('on-finished');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { AuthenticationClient, UserInfoClient } = require('auth0');
+const { auth } = require('express-oauth2-jwt-bearer');
 const port = 3000;
-const fs = require('fs');
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
+
+const CLIENT_ID = 'BG640TUfkyjyMdYg1a8ljHuhbVpuVqIe';
+const CLIENT_SECRET = 'klgAc7CW9Jv1SNqHl55VCrv1kX1joxlzKH50pxfpUbSUS9EE0WTRKqXkjeQd52rw';
+const DOMAIN = 'dev-oq87tp0qk6gh1qp8.us.auth0.com';
+
+const auth0 = new AuthenticationClient({
+    domain: DOMAIN,
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+});
+
+const checkJwt = auth({
+    audience: ' https://localhost:3000',
+    issuerBaseURL: `https://${DOMAIN}/`,
+});
+
+const userInfo = new UserInfoClient({
+    domain: DOMAIN,
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const SESSION_KEY = 'Authorization';
+const TOKEN_KEY = 'Authorization';
 
-class Session {
-    #sessions = {}
-
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
-
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
-        }
+app.get('/api/me', checkJwt, async (req, res) => {
+    const auth = req.auth;
+    if (auth) {
+        const token = auth.token;
+        const { data } = await userInfo.getUserInfo(token);
+        console.log(data);
+        return res.json(data);
     }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
-}
-
-const sessions = new Session();
-
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
-
-    next();
+    return res.status(401).json({ 'error': 'Forbidden' });
 });
 
 app.get('/', (req, res) => {
-    if (req.session.username) {
-        return res.json({
-            username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
-    }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
-
-app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
-    res.redirect('/');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
+app.get('/api/redirect', (req, res) => {
+    return res.redirect(301, `https://${DOMAIN}/authorize?client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/&response_type=code&response_mode=query`);
+});
 
+app.get('/api/callback', async (req, res) => {
+    const code = req.query['code'];
+    if (code) {
+        const data = await auth0.oauth.authorizationCodeGrant({ code, redirect_uri: 'http://localhost:3000/', grant_type: 'authorization_code' });
+        console.log(data.data.access_token);
+        return res.json(data.data);
+    }
+    return res.status(403).json({ error: 'Forbidden' });
+});
 
 app.post('/api/login', async (req, res) => {
     const { login, password } = req.body;
 
     try {
-        // Make a request to Auth0 to authenticate the user
-        const authResponse = await axios.post('https://dev-oq87tp0qk6gh1qp8.us.auth0.com/oauth/token', {
-            grant_type: 'password',
+        const { data } = await auth0.oauth.passwordGrant({
             username: login,
-            password: password,
-            client_id: 'BG640TUfkyjyMdYg1a8ljHuhbVpuVqIe',
-            client_secret: 'klgAc7CW9Jv1SNqHl55VCrv1kX1joxlzKH50pxfpUbSUS9EE0WTRKqXkjeQd52rw',
-            audience: ' https://localhost:3000',
-            scope: 'openid profile email',
+            password,
+            scope: 'offline_access openid',
+            audience: ' https://localhost:3000'
         });
 
-        const accessToken = authResponse.data.access_token;
-
-        // You may want to decode the access token to get user information
-        const userInformation = jwt.decode(accessToken);
-
-        // Store user information in the session
-        req.session.username = userInformation.username;
-        req.session.login = userInformation.login;
-
-        // Send a response with the access token
-        res.json({ token: accessToken });
-    } catch (error) {
-        console.error(error);
-
-        // Handle authentication failure
-        res.status(401).json({ error: 'Authentication failed', message: 'Wrong email or password.' });
+        res.json(data).send();
+    } catch (err) {
+        res.status(401).send();
     }
+});
+
+app.post('/api/refresh', async (req, res) => {
+    const refresh_token = req.get(TOKEN_KEY);
+
+    const { data } = await auth0.oauth.refreshTokenGrant({ refresh_token });
+
+    res.json(data).send();
 });
 
 app.listen(port, () => {
